25c8b8973e1bab4dacd712c18e63dfc4ad10dcda
John Sener <john.sener@inspirationtech.co.uk>
2024-01-26 00:22:33 +0000

    first commit

---
+++ PaginationCodeFirst/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaginationCodeFirst.Models;
+
+namespace PaginationCodeFirst.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ PaginationCodeFirst/Controllers/BooksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaginationCodeFirst.Context;
+using PaginationCodeFirst.Models;
+
+namespace PaginationCodeFirst.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index(int page = 1, int pageSize = 5)
+        {
+            var books = _context.Books
+                .OrderBy(b => b.Id)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var totalBooks = _context.Books.Count();
+            var totalPages = (int)Math.Ceiling((double)totalBooks / pageSize);
+
+            var viewModel = new PaginatedList<Book>(books, page, totalPages);
+
+            return View(viewModel);
+        }
+    }
+}
